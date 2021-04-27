dac270e3fe3620d54d51d2b607b95230f3ebe482
Riska Abdullah <alternative.xen@gmail.com>
2021-04-27 16:32:18 +0800

    prepare postman testing

--- api/app.py
+++ api/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_uploads import configure_uploads, patch_request_class
+from image_resource import ImageUpload
+from image_helper import IMAGE_SET
 
 
 def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
+    patch_request_class(app, 10 * 1024 * 2014)  # limit maximum size upload
+    configure_uploads(app, IMAGE_SET)
 
     from models import db
     db.init_app(app)
