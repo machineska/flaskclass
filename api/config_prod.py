bb157581ace6b32cb98373b9df9a47359fc1959e
Riska Abdullah <alternative.xen@gmail.com>
2021-05-04 07:32:46 +0800

    modify server name

--- api/config_prod.py
+++ api/config_prod.py
@@ -3,9 +3,9 @@ import os
 
 # You need to replace the next values with the appropriate values for your configuration
 basedir = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
-PORT = 5000
-HOST = "127.0.0.1"
+DEBUG = False
+PORT = 80
+HOST = "skaterska.pythonanywhere.com"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(
